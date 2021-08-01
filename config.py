1ad588bad06b80dd29f8a9777ac3dfa26387e436
traitor <himalrawal07@gmail.com>
2021-08-01 10:15:33 +0530

    initial commit

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ from app import app
 from flaskext.mysql import MySQL
 
 mysql = MySQL()
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = ''
-app.config['MYSQL_DATABASE_DB'] = 'hireme'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_USER'] = '3907626_hireme'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'Test@981077'
+app.config['MYSQL_DATABASE_DB'] = ' 3907626_hireme'
+app.config['MYSQL_DATABASE_HOST'] = 'fdb32.awardspace.net'
 mysql.init_app(app)
